8c377b6d1658c502e1f3a519fbe3afa82eba1e44
MARCELO ISRAEL ESTUPINAN MEJIA <mestupinan@riasem.com.ec>
2022-11-22 15:18:35 -0500

    Se agrega Get Localidad por identificación

--- src/Application/Features/Locacions/Queries/GetLocacion/GetLocacionAsyncQueries.cs
+++ src/Application/Features/Locacions/Queries/GetLocacion/GetLocacionAsyncQueries.cs
@@ -1,22 +1,25 @@
 ﻿using AutoMapper;
 using EvaluacionCore.Application.Common.Interfaces;
 using EvaluacionCore.Application.Common.Wrappers;
+using EvaluacionCore.Application.Features.Locacions.Specifications;
 using EvaluacionCore.Application.Features.Localidads.Dto;
 using EvaluacionCore.Domain.Entities.Asistencia;
 using MediatR;
 
 namespace EvaluacionCore.Application.Features.Localidads.Queries.GetLocalidad;
 
-public record GetLocalidadAsyncQueries(string IdLocalidad) : IRequest<ResponseType<List<LocalidadType>>>;
+public record GetLocalidadAsyncQueries(string IdLocalidad, string Identificacion) : IRequest<ResponseType<List<LocalidadType>>>;
 
 public class GetLocalidadAsyncQueriesHandler : IRequestHandler<GetLocalidadAsyncQueries, ResponseType<List<LocalidadType>>>
 {
     private readonly IRepositoryAsync<Localidad> _repositoryAsync;
+    private readonly IRepositoryAsync<LocalidadColaborador> _repositoryLcAsync;
     private readonly IMapper _mapper;
 
-    public GetLocalidadAsyncQueriesHandler(IRepositoryAsync<Localidad> repository, IMapper mapper)
+    public GetLocalidadAsyncQueriesHandler(IRepositoryAsync<Localidad> repository, IRepositoryAsync<LocalidadColaborador> repositoryLc, IMapper mapper)
     {
         _repositoryAsync = repository;
+        _repositoryLcAsync = repositoryLc;
         _mapper = mapper;
     }
 
@@ -29,20 +32,28 @@ public class GetLocalidadAsyncQueriesHandler : IRequestHandler<GetLocalidadAsync
             if (request.IdLocalidad is not null)
             {
                 objLocalidad = objLocalidad.Where(x => x.Id == Guid.Parse(request.IdLocalidad) && x.Estado == "A").ToList();
+            }
+            else if (request.Identificacion is not null)
+            {
+                var localidadcolaborador = await _repositoryLcAsync.ListAsync(new GetLocationByColaboradorSpec(request.Identificacion), cancellationToken);
+                objLocalidad = new List<Localidad>();
 
+                foreach (var lc in localidadcolaborador)
+                {
+                    objLocalidad.Add(lc.Localidad);
+                }
             }
             else
             {
                 objLocalidad = objLocalidad.Where(x => x.Estado == "A").ToList();
             }
-            return new ResponseType<List<LocalidadType>>() { Data = _mapper.Map<List<LocalidadType>>(objLocalidad), Message = "Consulta Generada exitosamente", StatusCode = "000", Succeeded = true };
 
+            return new ResponseType<List<LocalidadType>>() { Data = _mapper.Map<List<LocalidadType>>(objLocalidad), Message = "Consulta Generada exitosamente", StatusCode = "000", Succeeded = true };
         }
         catch (Exception e)
         {
             return new ResponseType<List<LocalidadType>>() { Data = null, Message = "Ocurrió un error durante la consulta", StatusCode = "002", Succeeded = false };
         }
-        
-        
+
     }
 }
---
+++ src/Application/Features/Locacions/Specifications/GetLocationByColaboradorSpec.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using EvaluacionCore.Domain.Entities.Asistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionCore.Application.Features.Locacions.Specifications
+{
+    public class GetLocationByColaboradorSpec : Specification<LocalidadColaborador>
+    {
+        public GetLocationByColaboradorSpec(string Identificacion)
+        {
+            Query.Where(p => p.Colaborador.Identificacion == Identificacion && p.Estado == "A")
+                .Include(p => p.Localidad);
+        }
+    }
+}
