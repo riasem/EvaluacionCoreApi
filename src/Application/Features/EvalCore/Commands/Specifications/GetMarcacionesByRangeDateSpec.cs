7a5122f4e40864862a1719016bd95a69bcec2a07
DouglasBor <92886968+DouglasBor@users.noreply.github.com>
2022-11-29 15:16:30 -0500

    Evaluacion con parametros

---
+++ src/Application/Features/EvalCore/Commands/Specifications/GetMarcacionesByRangeDateSpec.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Specification;
+using EnrolApp.Domain.Entities.Horario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionCore.Application.Features.EvalCore.Commands.Specifications;
+
+public class GetMarcacionesByRangeDateSpec : Specification<CheckInOut>
+{
+    public GetMarcacionesByRangeDateSpec(int userId, DateTime? fechaDesde, DateTime? fechaHasta)
+    {
+        Query.Where(x => x.UserId == userId && x.CheckTime.Date >= fechaDesde.Value.Date && x.CheckTime.Date <= fechaHasta.Value.Date)
+            .OrderByDescending(x => x.CheckTime);
+
+
+    }
+
+}
