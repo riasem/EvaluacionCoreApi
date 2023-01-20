9f63bfe3154b1d53131153bc8532df0b2d10550f
MarceGow <73322190+MarceGow@users.noreply.github.com>
2023-01-20 16:29:09 -0500

    Se modifica asignar turnos y subturnos

--- src/Application/Features/Turnos/Commands/CreateTurnoCliente/CreateTurnoColaboradorCommand.cs
+++ src/Application/Features/Turnos/Commands/CreateTurnoCliente/CreateTurnoColaboradorCommand.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using EvaluacionCore.Application.Common.Interfaces;
 using EvaluacionCore.Application.Common.Wrappers;
+using EvaluacionCore.Application.Features.Turnos.Specifications;
 using EvaluacionCore.Domain.Entities.Asistencia;
 using MediatR;
 
@@ -8,21 +9,19 @@ namespace EvaluacionCore.Application.Features.Turnos.Commands.CreateTurnoColabor
 
 public record CreateTurnoColaboradorCommand(CreateTurnoColaboradorRequest TurnoRequest) : IRequest<ResponseType<string>>;
 
-
 public class CreateTurnoColaboradorCommandHandler : IRequestHandler<CreateTurnoColaboradorCommand, ResponseType<string>>
 {
     private readonly IRepositoryAsync<TurnoColaborador> _repoTurnoAsync;
-    private readonly IMapper _mapper;
 
-    public CreateTurnoColaboradorCommandHandler(IRepositoryAsync<TurnoColaborador> repository, IMapper mapper)
+    public CreateTurnoColaboradorCommandHandler(IRepositoryAsync<TurnoColaborador> repository)
     {
         _repoTurnoAsync = repository;
-        _mapper = mapper;
     }
 
     public async Task<ResponseType<string>> Handle(CreateTurnoColaboradorCommand request, CancellationToken cancellationToken)
     {
-        DateTime fechaAsigna;
+        List<TurnoColaborador> turnos = new();
+        List<TurnoColaborador> subturnos = new();
 
         try
         {
@@ -30,23 +29,20 @@ public class CreateTurnoColaboradorCommandHandler : IRequestHandler<CreateTurnoC
             DateTime fechaHasta = request.TurnoRequest.FechaAsignacionHasta;
             TimeSpan difFechas = fechaHasta - fechaDesde;
 
-
-            for (int i = 0; i <= difFechas.Days; i++ )
+            for (int i = 0; i <= difFechas.Days; i++)
             {
+                var consulta = await _repoTurnoAsync.ListAsync(new GetTurnosByFechaAsignacionSpec(fechaDesde.AddDays(i)), cancellationToken);
+
                 foreach (var item in request.TurnoRequest.ClienteSubturnos)
                 {
-
-                    var consulta = await _repoTurnoAsync.ListAsync(cancellationToken);
-                    var filtro = consulta.Where(e => e.FechaAsignacion == fechaDesde.AddDays(i) && e.IdColaborador == item.IdCliente && e.Estado == "A").ToList();
+                    var filtro = consulta.Where(e => e.FechaAsignacion == fechaDesde.AddDays(i) && e.IdColaborador == item.IdCliente).ToList();
 
                     if (filtro.Count > 0)
-                    {
                         return new ResponseType<string>() { Data = null, Message = "Ya existen turnos asignados en el rango de las fechas indicadas", StatusCode = "101", Succeeded = false };
-                    }
-                    Guid guid = Guid.NewGuid();
+
                     TurnoColaborador objClient = new()
                     {
-                        Id = guid,
+                        Id = Guid.NewGuid(),
                         Estado = "A",
                         UsuarioCreacion = "SYSTEM",
                         IdTurno = request.TurnoRequest.IdTurno,
@@ -54,22 +50,15 @@ public class CreateTurnoColaboradorCommandHandler : IRequestHandler<CreateTurnoC
                         FechaAsignacion = fechaDesde.AddDays(i)
                     };
 
-                    var objResult = await _repoTurnoAsync.AddAsync(objClient, cancellationToken);
-
-                    if (objResult is null)
-                    {
-                        return new ResponseType<string>() { Data = null, Message = "No se pudo registrar la asignación del turno Laboral", StatusCode = "101", Succeeded = false };
-                    }
+                    turnos.Add(objClient);
 
                     if (item.Subturnos.Count > 0)
                     {
-
                         foreach (var item2 in item.Subturnos)
                         {
-                            Guid guid2 = Guid.NewGuid();
                             TurnoColaborador objClient2 = new()
                             {
-                                Id = guid2,
+                                Id = Guid.NewGuid(),
                                 Estado = "A",
                                 UsuarioCreacion = "SYSTEM",
                                 IdTurno = item2.IdSubturno,
@@ -77,25 +66,30 @@ public class CreateTurnoColaboradorCommandHandler : IRequestHandler<CreateTurnoC
                                 FechaAsignacion = fechaDesde.AddDays(i)
                             };
 
-                            var objResult2 = await _repoTurnoAsync.AddAsync(objClient2, cancellationToken);
-
-                            if (objResult2 is null)
-                            {
-                                return new ResponseType<string>() { Data = null, Message = "No se pudo registrar la asignación del (los) turno(s) de receso", StatusCode = "101", Succeeded = false };
-                            }
+                            subturnos.Add(objClient2);
                         }
                     }
                 }
             }
 
-            
-            return new ResponseType<string>() { Data = /*objResult.Id.ToString()*/ null,Message = "Turnos asignados correctamente", StatusCode ="100",Succeeded = true };
+            var resTurnos = await _repoTurnoAsync.AddRangeAsync(turnos, cancellationToken);
+
+            if (!resTurnos.Any())
+                return new ResponseType<string>() { Data = null, Message = "No se pudo registrar la asignación del turno Laboral", StatusCode = "101", Succeeded = false };
+
+            if (subturnos.Any())
+            {
+                var resSubturnos = await _repoTurnoAsync.AddRangeAsync(subturnos, cancellationToken);
+
+                if (!resSubturnos.Any())
+                    return new ResponseType<string>() { Data = null, Message = "No se pudo registrar la asignación del (los) turno(s) de receso", StatusCode = "101", Succeeded = false };
+            }
+
+            return new ResponseType<string>() { Data = null, Message = "Turnos asignados correctamente", StatusCode = "100", Succeeded = true };
         }
-        catch (Exception ex)
+        catch (Exception)
         {
             return new ResponseType<string>() { Data = null, Message = "No se pudo registrar la asignación", StatusCode = "102", Succeeded = false };
         }
-        
-       
     }
 }
---
+++ src/Application/Features/Turnos/Specifications/GetTurnosByFechaAsignacionSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using EvaluacionCore.Domain.Entities.Asistencia;
+
+namespace EvaluacionCore.Application.Features.Turnos.Specifications
+{
+    public class GetTurnosByFechaAsignacionSpec : Specification<TurnoColaborador>
+    {
+        public GetTurnosByFechaAsignacionSpec(DateTime fechaAsignacion)
+        {
+            Query.Where(p => p.FechaAsignacion.Date == fechaAsignacion.Date && p.Estado == "A");
+        }
+    }
+}
