b0f522f464d9c223b38ebb92b5ee4b41220aca09
DouglasBor <109366031+riasem@users.noreply.github.com>
2023-02-08 10:17:49 -0500

    captura de excepcion por rol cargo

--- src/Application/Features/EvalCore/Queries/GetEvaluacionAsistenciaAsync/GetEvaluacionAsistenciaAsyncQuery.cs
+++ src/Application/Features/EvalCore/Queries/GetEvaluacionAsistenciaAsync/GetEvaluacionAsistenciaAsyncQuery.cs
@@ -74,20 +74,30 @@ public class GetEvaluacionAsistenciaAsyncHandler : IRequestHandler<GetEvaluacion
             var colaSesion = await _repoColabConvivenciaAync.FirstOrDefaultAsync(new GetColaboradorConvivenciaByUdnAreaSccSpec("", "", "", request.identSession));
 
             var rolCargo = await _repoRolCargoAync.FirstOrDefaultAsync(new GetRolesAccesoByCargoConvivenciaSpec(colaSesion.CodCargo, colaSesion.CodSubcentroCosto, request.idCanal, ""));
-            var listAttributos = await _repoAtributoRolAync.ListAsync(new GetAtributosByRolSpec(rolCargo.RolSG.Id));
 
-            var idAtributoTTHH = _config.GetSection("Atributos:ControlAsistenciaTTHH").Get<string>();
-
-            var atributoTTHH = listAttributos.Where(x => x.Id == Guid.Parse(idAtributoTTHH)).ToList();
-            if (!atributoTTHH.Any())
+            if (rolCargo != null)
             {
-                var objJefe = await _repoClienteAync.FirstOrDefaultAsync(new GetColaboradorByIdentificacionSpec(colaSesion.Identificacion));
-                var objColaboradoresJefe = await _repoClienteAync.ListAsync(new GetColaboradorByJefe(objJefe.Id));
+                var listAttributos = await _repoAtributoRolAync.ListAsync(new GetAtributosByRolSpec(rolCargo.RolSG.Id));
+
+                var idAtributoTTHH = _config.GetSection("Atributos:ControlAsistenciaTTHH").Get<string>();
+
+                var atributoTTHH = listAttributos.Where(x => x.Id == Guid.Parse(idAtributoTTHH)).ToList();
+                if (!atributoTTHH.Any())
+                {
+                    var objJefe = await _repoClienteAync.FirstOrDefaultAsync(new GetColaboradorByIdentificacionSpec(colaSesion.Identificacion));
+                    var objColaboradoresJefe = await _repoClienteAync.ListAsync(new GetColaboradorByJefe(objJefe.Id));
 
 
-                cabecera = cabecera.Where(x => objJefe.Identificacion == x.Identificacion || objColaboradoresJefe.Any(c => c.Identificacion == x.Identificacion)).ToList();
+                    cabecera = cabecera.Where(x => objJefe.Identificacion == x.Identificacion || objColaboradoresJefe.Any(c => c.Identificacion == x.Identificacion)).ToList();
 
+                }
             }
+            else
+            {
+                cabecera = cabecera.Where(x => x.Identificacion == colaSesion.Identificacion).ToList();
+            }
+
+
 
             #endregion
 
