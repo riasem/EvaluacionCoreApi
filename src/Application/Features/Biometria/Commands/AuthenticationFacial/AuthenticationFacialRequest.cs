7e3760a31b37efb28044fcce57c7230587fde32f
DouglasBor <109366031+riasem@users.noreply.github.com>
2023-04-04 11:52:21 -0500

    ok

--- src/Application/Features/Biometria/Commands/AuthenticationFacial/AuthenticationFacialRequest.cs
+++ src/Application/Features/Biometria/Commands/AuthenticationFacial/AuthenticationFacialRequest.cs
@@ -5,6 +5,8 @@
         public string Base64 { get; set; } = string.Empty;
         public string Nombre { get; set; } = string.Empty;
         public string Extension { get; set; } = string.Empty;
+
+        public string FacialPersonUid { get; set; } = string.Empty;
         public string Identificacion { get; set; } = string.Empty;
     }
 }
