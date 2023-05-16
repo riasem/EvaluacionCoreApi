671e724703d90023ab1ceedbd51076524597bd7b
DouglasBor <109366031+riasem@users.noreply.github.com>
2023-05-16 14:36:06 -0500

    Luxand Api Key

--- src/Persistence/Repository/Biometria/BiometriaService.cs
+++ src/Persistence/Repository/Biometria/BiometriaService.cs
@@ -180,7 +180,7 @@ namespace Workflow.Persistence.Repository.Biometria
 
 
                 #region Luxand
-                FSDK.ActivateLibrary("ITP46F7LrqUL03MbPpqoxAQ3QhzK6ABcKMPz/FmBnvEPDMyZy6mYtrEgyNe3vk3h55QtpOr+ANaQYG6GMy3PNJmfmjTuwI9r3+ioyOBlb/mPgCdEkM1MXwogISqCSuKwGUvr9gPpPHZmTZyjjFMSowuiBrigIOvjtU50vh/LwaI=");
+                FSDK.ActivateLibrary("aTnuLhSRuOXFwOZq9IBWpBIQMF0RMoBI/gNFdS3aG3eNatqoJssib09CHdS/0OS4QCzTzH1dXYQvdyL6qmKnaiAEBMCSoK01AL0dNc33eczA2fi3v3hiHRShP3v9EtG+RG07m1oY++kAFB5qJYOs7WS1iVRkQbdK2LUCNJBUsO4=");
                 FSDK.InitializeLibrary();
 
                 FSDK.CImage imageCola = new FSDK.CImage(objColaborador.ImagenPerfil.RutaAcceso.ToString());
