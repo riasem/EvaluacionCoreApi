2aaee4fc5340935dcabda3ea4a8664383866ceb8
riasem <tics@riasem.com.ec>
2023-08-29 23:11:12 -0500

    Prueba con la ultima licencia que no era ilimitada

--- src/Persistence/Repository/Biometria/BiometriaService.cs
+++ src/Persistence/Repository/Biometria/BiometriaService.cs
@@ -87,7 +87,9 @@ namespace Workflow.Persistence.Repository.Biometria
                             if (objColaborador is null) return new ResponseType<string>() { Data = null, Message = "Colaborador no tiene Imagen de Perfil", StatusCode = "101", Succeeded = true };
 
 //                            var xxx = FSDK.ActivateLibrary(Licencia.CodigoLicencia);
-                            var xxx = FSDK.ActivateLibrary("JOassJsQHq6XVRdg5bUgLyUsSEXDS3qPFSRfzJ9WYMSrPYmp7TCFiytbKyVOZZeFacobAJyN1zRqPNIX2mBVJQERs3s4EIyU5b7Eb7UcjG8Tx+ovF2hw8HuktW+vQuuxp8txaYZcc4nL4oi2y+3gTPTzmXn+6YoLPvr5ZEWJ+XQ=");
+//                            var xxx = FSDK.ActivateLibrary("JOassJsQHq6XVRdg5bUgLyUsSEXDS3qPFSRfzJ9WYMSrPYmp7TCFiytbKyVOZZeFacobAJyN1zRqPNIX2mBVJQERs3s4EIyU5b7Eb7UcjG8Tx+ovF2hw8HuktW+vQuuxp8txaYZcc4nL4oi2y+3gTPTzmXn+6YoLPvr5ZEWJ+XQ=");
+                            var xxx = FSDK.ActivateLibrary("YK6tt2AQmhevRUTW9hDnev5pB14PEjdaSzXfchF8Z/cJix53l2mt38mNEJUkfXPmWQ8TKQyZQsXlLRFiVkgrDj86co0xYLhoJltayZlea1zmqyzaN/yre+zOqEyr/1fDXLWkE4MEoQY8eOpj6hCrRsDP10EkunTtiz6mC8ar6AU=");
+
                             string hardwareID;
                             var www = FSDK.GetHardware_ID(out hardwareID);
                             int num = 0;
