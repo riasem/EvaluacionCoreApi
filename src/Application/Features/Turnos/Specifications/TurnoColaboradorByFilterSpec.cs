9b98363c570789bf4af36ccfb3f66cd46754c8a1
kpincay <38895560+kpincay@users.noreply.github.com>
2024-01-22 11:01:01 -0500

    Modificado para que tome el turno del Jefe además de los de los colaboradores
    Modificado para que tome el turno del Jefe además de los de los colaboradores

--- src/Application/Features/Turnos/Specifications/TurnoColaboradorByFilterSpec.cs
+++ src/Application/Features/Turnos/Specifications/TurnoColaboradorByFilterSpec.cs
@@ -8,7 +8,7 @@ public class TurnoColaboradorByFilterSpec : Specification<TurnoColaborador>
     public TurnoColaboradorByFilterSpec(Guid Colaborador, DateTime FechaDesde, DateTime FechaHasta)
     {
         Query.Where(e =>  e.FechaAsignacion >= FechaDesde  && e.FechaAsignacion <= FechaHasta && e.Estado == "A")
-             .Include(p => p.Colaborador).Where(p => p.Colaborador.ClientePadreId == Colaborador);
+             .Include(p => p.Colaborador).Where(p => p.Colaborador.ClientePadreId == Colaborador || p.Colaborador.Id == Colaborador);
             
     }
 }
